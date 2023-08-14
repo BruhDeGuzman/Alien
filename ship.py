d0f538142a3db7ec96b86dcd66f5dca2f637c64d
Ali Jabar <ali93456@gmail.com>
2023-08-14 04:20:33 +0300

    edit the image path after moving it

--- ship.py
+++ ship.py
@@ -9,7 +9,7 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
 
         # load the game image and get it's rect
-        self.image = pygame.image.load('ship.bmp')
+        self.image = pygame.image.load('assets/ship.bmp')
         self.rect = self.image.get_rect()
 
         #start each new ship at the bottom center of the screen
